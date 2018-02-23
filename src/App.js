47b83d5d9d0958f9868cbbdaad235cb9eafb9d6e
VLaroye <33655713+VLaroye@users.noreply.github.com>
2018-02-23 21:06:08 +0100

    Initial commit. create-react-app + Eject + Config to use CSS modules + Clean Up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App} >
+      </div>
+    );
+  }
+}
+
+export default App;
