711ff041b12b0df7d1bb474787217c56b4831bf1
VLaroye <vlaroye@cepensis.fr>
2018-05-25 14:03:58 +0200

    Artist page

--- src/scenes/Artist/Artist.js
+++ src/scenes/Artist/Artist.js
@@ -1,9 +1,136 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/fontawesome-free-brands';
+
+// FIREBASE 
+import firebase from 'firebase';
+
+// COMPONENTS
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ArtistImg = styled.img`
+  width: 50%;
+  max-height: 15em;
+`;
+
+const ArtistDetails = styled.div`
+  width: 50%
+  display: flex;
+  flex-wrap: wrap;
+
+  #origine {
+    width: 50%;
+    h4 {
+      margin: 0;
+      padding: 0.2em 0 0.2em 0.5em;
+      font-family: Ubuntu;
+      font-weight: bold;
+      text-transform: uppercase;
+      width: 100%;
+      background-color: #b14772;
+    }
+  }
+
+  #genre {
+    width: 50%;
+    h4 {
+      margin: 0;
+      padding: 0.2em 0 0.2em 0.5em;
+      font-family: Ubuntu;
+      font-weight: bold;
+      text-transform: uppercase;
+      color: white;
+      background-color: black;
+    }
+  }
+
+  #links {
+    width: 100%;
+
+    h4 {
+      margin: 0;
+      padding: 0.2em 0 0.2em 0.5em;
+      font-family: Ubuntu;
+      font-weight: bold;
+      text-transform: uppercase;
+      width: 100%;
+      background-color: #b14772;
+    }
+  }
+
+  #description {
+    width: 100%;
+    h4 {
+      margin: 0;
+      padding: 0.2em 0 0.2em 0.5em;
+      font-family: Ubuntu;
+      font-weight: bold;
+      text-transform: uppercase;
+      color: white;
+      background-color: black;
+    }
+  }
+`;
 
 class Artist extends Component {
+  state = {
+    artist: [],
+    loading: true,
+    error: 'Impossible de trouver cet artiste',
+  }
+
+  componentDidMount() {
+    var database = firebase.database();
+    let artists = { ...this.state.artists };
+    var artistsRef = database.ref('/artists');
+
+    artistsRef.once('value')
+      .then(data => {
+        artists = data.val();
+        for(var i = 0; i < artists.length; i++) {
+          if (artists[i] != undefined && artists[i].name && artists[i].name === this.props.match.params.artistName) {
+            console.log(artists[i]);
+            this.setState({artist : artists[i]});
+            break;
+          }
+        }
+      });
+  };
+
   render() {
     return (
-      <div>Artist</div>
+      <div>
+        <SectionTitle>{ this.state.artist.name }</SectionTitle>
+        <Container>
+          <ArtistImg src={this.state.artist.imgSrc}/>
+          <ArtistDetails>
+            <div id="origine">
+              <h4>Origine</h4>
+              <p>{ this.state.artist.origin || "N.C" }</p>
+            </div>
+            <div id="genre">
+              <h4>Genre</h4>
+              <p>{ this.state.artist.style || "N.C" }</p>
+            </div>
+            <div id="links">
+              <h4>Liens</h4>
+              <div id="linksContainer">
+                <div>{ this.state.artist.name }</div>
+              </div>
+            </div>
+            <div id="description">
+              <h4>Description</h4>
+              <p>{ this.state.artist.description || "N.C" }</p>
+            </div>
+          </ArtistDetails>
+        </Container>
+      </div>
     );
   }
 }
